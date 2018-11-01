183f17cb5732f9ce06b444f55b74cd62e8052939
borisowsky <yuzirock@gmail.com>
2018-11-01 19:54:12 +0200

    Add react-hot-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { hot } from 'react-hot-loader';
 
-class App extends Component {
-  render() {
-    return <div>Hello world</div>;
-  }
-}
+const App = () => <div>Hello world!</div>;
 
-export default App;
+export default hot(module)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-if ((module as any).hot) {
-  (module as any).hot.accept();
-}
