d8015d3584fccfa19c08d8214d695beb8d1422ba
Nikita Borisowsky <yuzirock@gmail.com>
2019-08-12 13:26:12 +0200

    Fix assertion

--- src/e2e/initial-screen.spec.ts
+++ src/e2e/initial-screen.spec.ts
@@ -3,5 +3,5 @@
 it('Open main page, and see "Hello world!" message', () => {
   cy.visit('http://localhost:3000');
 
-  cy.contains('Hello world!');
+  cy.get('#root').should('contain.text', 'Hello world!');
 });
